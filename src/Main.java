98f713e0663c7e6f391ddd477114be8077bccf28
Shraddhars24 <shraddhars24@gmail.com>
2025-06-04 17:36:31 -0500

    Second Commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 15; i++) {
             System.out.println("Hello, World!");
             try {
                 Thread.sleep(2000); // Sleep for 1 second
